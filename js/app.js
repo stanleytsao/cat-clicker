09cc79a7fb3066a20abc8248f4d200f42a0058e6
Stanley Tsao <stsao002@gmail.com>
2016-12-07 14:56:41 -0800

    built layout for cat-clicker premium

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 $cats = 2;
 
 var catPics = ['garfield.jpg', 'meowth.png', 'simba.png', 'sylvester.png', 'tom.png'];
-var $container = $('.container');
+var $selector = $('.cat-selector');
+var $content = $('.content');
 
-function builder(cats) {
+function selectorBuilder() {
+	
+
+	catPics.forEach(function (cat) {
+		$selector.append('<div class="selection"><img class="icon" src="img/' + cat + '"><h3 class="catName">' + cat.slice(0,-4) + '</h3></div>');
+	})
+};
+selectorBuilder();
+
+function counterBuilder(cats) {
 	for (var i = 0; i < cats; i++) {
-		$container.append('<div class="catBox catBox' + i + '"><div class="counter' + i + '">Clicks: 0</div><img class="cat" src="img/' + catPics[Math.round(Math.random() * 4)] + '"></div>');
+		$content.append('<div class="catBox catBox' + i + '"><div class="counter' + i + '">Clicks: 0</div><img class="cat" src="img/' + catPics[Math.round(Math.random() * 4)] + '"></div>');
 	};
 };
-builder($cats);
+counterBuilder($cats);
 	
 $('.catBox').click(function(e) {
 	var $counter = this.firstChild;
