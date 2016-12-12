bda501691076e7b7f33fe215d837bf0c50389227
Stanley Tsao <stsao002@gmail.com>
2016-12-12 10:39:03 -0800

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,57 +1,12 @@
-var model = {
-    cats : [
-        {
-            name : 'Garfield',
-            img : 'img/garfield.jpg',
-            clicks : 0
-        },
-        {
-            name : 'Meowth',
-            img : 'img/meowth.png',
-            clicks : 0
-        },
-        {
-            name : 'Simba',
-            img : 'img/simba.png',
-            clicks : 0
-        },
-        {
-            name : 'Sylvester',
-            img : 'img/sylvester.png',
-            clicks : 0
-        },
-        {
-            name : 'Tom',
-            img : 'img/tom.png',
-            clicks : 0
-        }
-    ]
-};
+var ViewModel = function () {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Garfield');
+    this.imgSrc = ko.observable('img/garfield.jpg');
+    // this.imgAttribution = ko.observable();
 
-var octopus = {
-
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
 }
 
-var catView = {
-    init :  function() {
-        // body...
-    }
-    render : function() {
-        // body...
-    }
-};
-
-var listView = {
-    init :  function() {
-        // body...
-    }
-    render : function() {
-        // body...
-    }
-};
-
-// octopus.init();
-
-console.log(model);
-
-
+ko.applyBindings(new ViewModel())
